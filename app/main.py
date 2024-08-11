2719c60132eb4a11fb8fbd9adccf138bacc03791
Abbasa5251 <abbasa5251@hotmail.com>
2024-08-11 11:39:47 +0530

    stage-cat-file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import zlib
 
 def main():
     command = sys.argv[1]
@@ -11,6 +11,20 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+
+    elif command == "cat-file":
+        if sys.argv[2] == "-p":
+            commit_sha = sys.argv[3]
+            file_path = os.path.join(os.getcwd(), '.git', 'objects', commit_sha[:2], commit_sha[2:])
+
+            if not os.path.exists(file_path):
+                raise Exception(f'Not a valid object name {commit_sha}')
+            
+            with open(file_path, 'rb') as f:
+                raw = zlib.decompress(f.read())
+                _, content = raw.split(b"\0", maxsplit=1)
+                sys.stdout.write(content.decode())
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
